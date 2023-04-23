926b27e1831e59af7888dae24eebb4bc50d65f03
Likes learning-Liu <125553985+Liugaga-vvv@users.noreply.github.com>
2023-04-23 16:06:02 +0800

    Update check_room_availability.py

--- python/challenge-hotel-room-availability-checker/solutions/check_room_availability.py
+++ python/challenge-hotel-room-availability-checker/solutions/check_room_availability.py
@@ -5,7 +5,7 @@ def check_room_availability(room_types, rooms_available):
     for i in range(len(room_types)):
         # Check if there are 0 rooms available for a particular room type. if there are none, print "no <room type> rooms available".
         if rooms_available[i] == 0:
-            print(f"no {room_types[i]} rooms available")
+            print(f"No {room_types[i]} rooms available")
         # Check if there is one room available for a particular room type. if there's one, print "1 <room type> room available".
         elif rooms_available[i] == 1:
             print(f"1 {room_types[i]} room available")
