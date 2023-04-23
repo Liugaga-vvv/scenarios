a2d4b16ec61787f561b45f688fac2ec2f311c990
Likes learning-Liu <125553985+Liugaga-vvv@users.noreply.github.com>
2023-04-23 10:56:36 +0800

    Update calculate_bmi.py

--- python/challenge-body-mass-index-calculator/solutions/calculate_bmi.py
+++ python/challenge-body-mass-index-calculator/solutions/calculate_bmi.py
@@ -1,6 +1,6 @@
-# define a function called "calculate_bmi" which takes two parameters, weight and height.
+# Define a function called "calculate_bmi" which takes two parameters, weight and height.
 def calculate_bmi(weight, height):
-    # calculate the bmi using the formula weight divided by height squared.
+    # Calculate the bmi using the formula weight divided by height squared.
     bmi = weight / (height * height)
-    # round the calculated bmi value to two decimal places using the built-in round function.
+    # Round the calculated bmi value to two decimal places using the built-in round function.
     return round(bmi, 2)
