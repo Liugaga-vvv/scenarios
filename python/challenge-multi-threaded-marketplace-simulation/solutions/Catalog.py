e5037a8bcda99369088820903a3bd3955053ddf9
“githubLiugaga-vvv <GitHub978416202@qq.com>
2023-04-28 13:59:02 +0800

    add challenge-multi-threaded-marketplace-simulation

--- python/challenge-multi-threaded-marketplace-simulation/solutions/Catalog.py
+++ python/challenge-multi-threaded-marketplace-simulation/solutions/Catalog.py
@@ -1,37 +1,52 @@
 import threading
 from typing import Dict, List, Optional
 
-
+# defines a product class with id, name and price attributes 
 class Product:
     def __init__(self, id: int, name: str, price: float):
         self.id = id
         self.name = name
         self.price = price
 
+# defines a catalog class that stores products in a dictionary called `products` and provides methods to manipulate it
 class Catalog:
     def __init__(self):
+        # initialize an empty dictionary 'products' to store product objects keyed by their ids.
         self.products: Dict[int, Product] = {}
+        # initialize a lock for the catalog to prevent race conditions in multi-threaded usage
         self.lock = threading.Lock()
 
+    # method to add a product to the catalog's `products` dictionary
     def add_product(self, product: Product) -> None:
+        # use a lock to ensure this block of code is executed as one atomic operation to avoid race conditions.
         with self.lock:
             self.products[product.id] = product
 
+    # method to remove a specific product from the catalog by its id value
     def remove_product(self, product_id: int) -> None:
-        with self.lock:
+        # use a lock to ensure this block of code is executed as one atomic operation to avoid race conditions.
+         with self.lock:
             del self.products[product_id]
 
+    # method to retrieve a product object stored in the catalog given a specific product id
+    # this method returns none if the product is not found in the catalog.
     def get_product(self, product_id: int) -> Optional[Product]:
+        # use a lock to ensure this block of code is executed as one atomic operation to avoid race conditions.
         with self.lock:
             return self.products.get(product_id)
 
+    # method to return all products stored in the catalog as a list of product objects.
     def browse_catalog(self) -> List[Product]:
+        # use a lock to ensure this block of code is executed as one atomic operation to avoid race conditions.
         with self.lock:
             return list(self.products.values())
 
+    # method to display all products stored in the catalog formatted for human-readable output. 
     def display_catalog(self) -> None:
+        # use a lock to ensure this block of code is executed as one atomic operation to avoid race conditions.
         with self.lock:
             print("Catalog:")
+            # iterate over all product objects stored in the catalog and print their id, name
             for product in self.products.values():
                 print(f"ID: {product.id}, Name: {product.name}, Price: ${product.price}")
 
--- python/challenge-multi-threaded-marketplace-simulation/solutions/Seller_Buyer.py
+++ python/challenge-multi-threaded-marketplace-simulation/solutions/Seller_Buyer.py
@@ -3,22 +3,27 @@ import time
 from typing import List
 from Catalog import Catalog
 
+# defines a product class with id, name and price attributes 
 class Product:
     def __init__(self, id: int, name: str, price: float):
         self.id = id
         self.name = name
         self.price = price
 
-
 class Seller(threading.Thread):
+    # the seller class inherits from threading.thread and has an identifier and a catalog attribute.
     def __init__(self, id: int, catalog: Catalog):
         super().__init__()
         self.id = id
         self.catalog = catalog
 
+    # this method adds new products to the product catalog.
     def add_product(self, product: Product) -> None:
         self.catalog.add_product(product)
 
+    # the run method creates three new products with names and prices that are incremented in multiples of 10.
+    # each product is then added to the product catalog and prints a message to show that it was added.
+    # the thread sleeps for one second after each product is added.
     def run(self) -> None:
         for i in range(1, 4):
             product = Product(i, f"Product {i}", i * 10.0)
@@ -27,27 +32,37 @@ class Seller(threading.Thread):
             time.sleep(1)
 
 class Buyer(threading.Thread):
+    # define __init__ method to set instance variables id and catalog.
     def __init__(self, id: int, catalog: Catalog):
         super().__init__()
         self.id = id
         self.catalog = catalog
 
+    # define a method browse_catalog which calls the browse_catalog method of self.catalog.
+    # the return type is list of product type. 
     def browse_catalog(self) -> List[Product]:
         return self.catalog.browse_catalog()
 
+    # define a method buy_product which accepts an integer product_id as argument.
+    # this method tries to get a product from self.catalog with the help of get_product method.
+    # if product exists then print a message to console mentioning buyer id and name of product.
+    # finally remove that product from catalog.
     def buy_product(self, product_id: int) -> None:
         product = self.catalog.get_product(product_id)
         if product:
             print(f"Buyer {self.id} bought {product.name}")
             self.catalog.remove_product(product_id)
 
+    # define run method which is overriden from the thread class.
+    # sleep for 2 seconds, call browse_catalog and store the results in variable.
+    # iterate over each product, call buy_product method and sleep for 1 second.
     def run(self) -> None:
         time.sleep(2)
         products = self.browse_catalog()
         for product in products:
             self.buy_product(product.id)
             time.sleep(1)
-
+            
 if __name__ == "__main__":
     catalog = Catalog()
     sellers = [Seller(i, catalog) for i in range(1, 4)]
